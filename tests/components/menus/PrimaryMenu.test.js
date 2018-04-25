20a908a986be9557db895ec79c3379d34f081a44
entymon <pawel.olejniczak.i@gmail.com>
2018-04-25 10:25:57 +0100

    B-1 Added middlewar component for authorization

--- tests/components/menus/PrimaryMenu.test.js
+++ tests/components/menus/PrimaryMenu.test.js
@@ -3,6 +3,9 @@ import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import PrimaryMenu from '../../../app/components/menus/PrimaryMenu';
+import configureStore from '../../../app/store/configureStore';
+
+const store = configureStore();
 
 configure({ adapter: new Adapter() });
 
@@ -10,10 +13,10 @@ describe('PrimaryMenu Component', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<PrimaryMenu />);
+    wrapper = shallow(<PrimaryMenu store={store}/>);
   });
 
-  // it('should exist', () => {
-  //   expect(wrapper).toBeTruthy();
-  // });
+  it('should exist', () => {
+    expect(wrapper).toBeTruthy();
+  });
 });
